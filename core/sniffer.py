948b9660c2528e06f357455898465799c2a9fcc4
Arnab <arnabchy424@gmail.com>
2026-05-19 02:48:50 +0545

    Updated Sniffer.py

--- core/sniffer.py
+++ core/sniffer.py
@@ -1,20 +1,38 @@
 import os
 import socket
 
-class NetwrokSnifferr:
-    def __init__(self, host_ip: str) -> None:
+class NetworkSnifferr:
+    def __init__(self, host_ip) -> None:
         self.host_ip = host_ip
-        self.socekt = None
-
-    def initialize_socket(self, ip, port):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW)
-        self.ip = ip
-        self.port = port
-        client, add = sock.bind(ip, port)
+        self.sock = None
 
     def initialize_raw_socket(self, ip, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
         self.ip = ip
         self.port = port
-        sock.bind(ip, port)
-        self.sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+        self.sock = sock
+        sock.bind((ip, port))
+        sock.ioctl(socket.SIO_RCVALL, 1)
+        
+    def start_capture(self):
+        print("PRESS CTRL+C TO EXIT")
+        try:
+            count = 0
+            while True:
+                self.packet_data = self.sock.recvfrom(65535)
+                print(self.packet_data)
+                
+                count += 1
+                if count >= 10:
+                    break
+        except KeyboardInterrupt as e:
+            print("EXited Successfully")    
+
+ip = '192.168.56.1'
+port = 0
+
+
+if __name__ == "__main__":
+    net = NetworkSnifferr(ip)
+    net.initialize_raw_socket(ip, port)
+    net.start_capture()
